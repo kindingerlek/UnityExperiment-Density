c722d7ca4b79410a67cbc46527db9ea50a25323b
kindingerlek <kindingerlek@hotmail.com>
2018-03-20 22:43:39 -0300

    add extension methods like Volume to Vector3

---
+++ Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtensionMethods
+{
+    public static float Area(this Vector2 v)
+    {
+        return v.x * v.y;
+    }
+
+    public static float Area(this Vector2Int v)
+    {
+        return v.x * v.y;
+    }
+
+    public static float Volume(this Vector3 v)
+    {
+        return v.x * v.y * v.z;
+    }
+
+    public static float Volume(this Vector3Int v)
+    {
+        return v.x * v.y * v.z;
+    }
+}
